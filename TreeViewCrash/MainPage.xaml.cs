fdea9af5c840d9e24655f5394c5145e7dc8584a1
lukaszorg <lukasz.organisciak@blstream.com>
2019-05-28 11:07:06 +0200

    initial

---
+++ TreeViewCrash/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TreeViewCrash
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigationButton_Click(object sender, RoutedEventArgs e)
+        {
+            CrashFrame.Navigate(typeof(TreeViewPage));
+        }
+    }
+}
---
+++ TreeViewCrash/TreeViewPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TreeViewCrash
+{
+    public sealed partial class TreeViewPage : Page
+    {
+        public IEnumerable<int> DemoItemsSource = Enumerable.Range(0, 150);
+        public TreeViewPage()
+        {
+            Loaded += TreeViewPage_Loaded;
+            this.InitializeComponent();
+        }
+
+        private void TreeViewPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var node = new Microsoft.UI.Xaml.Controls.TreeViewNode();
+
+            foreach (var item in DemoItemsSource)
+            {
+                node.Children.Add(new Microsoft.UI.Xaml.Controls.TreeViewNode() { Content = item });
+            }
+
+            asd.RootNodes.Add(node);
+        }
+    }
+}
